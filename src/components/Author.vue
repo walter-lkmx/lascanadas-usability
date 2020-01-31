06953197a999728842b895f46d66b9500a1508ac
Walter Hurtado <walter.hurtado@lkmx.io>
2020-01-31 15:26:02 -0800

    .

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -3,9 +3,9 @@
 
 		<g-image alt="Author image" class="author__image" src="~/assets/images/lkmx_logo.svg" width="190" height="64" blur="0" />
 
-		<h1 v-if="showTitle" class="author__site-title" on-click="ga('send', 'event', 'home', 'titulo página', 'click en título')">
+		<a href="" v-if="showTitle" class="author__site-title" on-click="ga('send', 'event', 'home', 'titulo página', 'click en título');">
 			{{ $static.metadata.siteName }}
-		</h1>
+		</a>
 <section class="author__intro">
 	<h2>Lee antes de iniciar</h2>
 	<p>
